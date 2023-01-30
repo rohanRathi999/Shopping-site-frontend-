4c8be0a1219b9e0080b3d6f04606b82897fb63d6
Rohan Rathi <103847468+rohanRathi999@users.noreply.github.com>
2023-01-30 23:47:28 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const navitems = document.getElementsByClassName('nav-items')[0]
 
+toggleBtn.addEventListener('click', () => {
+  navitems.classList.toggle('active')
+})
